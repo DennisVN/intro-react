3b02096653ff2d6876608ab184fe6e188bf4ea6b
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-31 19:35:35 +0200

    Take input, handleAddTodo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList'
 import Form from './Form';
 
 function App() {
   // todos: every single todo inside the list
   // setTodos : function to update those 
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return
+    console.log(name)
+  }
+
   return (
     <>
     <TodoList todos={todos}/> 
-    <Form />
+    <div>
+        <input ref={todoNameRef} type="text" placeholder="Write a new todo" />
+        <br />
+        <button onClick={handleAddTodo}>Add todo</button>
+      </div>
     </>
   )
 }
--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,10 @@
 import React from "react";
 
 export default function Form() {
-function clickHandler() {
-    document.getElementById("click").addEventListener("click", clickHandler)
-}
+
   return (
     <>
-        <div>
-        <input type="text" placeholder="Write a new todo" />
-        <br />
-        <button id="click" onClick={clickHandler}>Add todo</button>
-        </div>
+        <p>i display Form.js</p>
     </>
   );
 }
