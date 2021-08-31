373a2bcab41c90aa5e9b92942815aba313904ca8
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-31 19:18:39 +0200

    Remove dummy object, WIP create new array from input

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Form from './Form';
 function App() {
   // todos: every single todo inside the list
   // setTodos : function to update those 
-  const [todos, setTodos] = useState([ { id: 1, name: 'Todo 1', complete: true }])
+  const [todos, setTodos] = useState([])
   return (
     <>
     <TodoList todos={todos}/> 
