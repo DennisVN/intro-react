a377eb2f8241134cf91005afbd2167a89867dbdc
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-31 19:07:08 +0200

    Print out Todo on screen as string

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import TodoList from './TodoList'
 import Form from './Form';
 
 function App() {
+  // todos: every single todo inside the list
+  // setTodos : function to update those 
   const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
     <>
-    <TodoList todos={todos}/>
+    <TodoList todos={todos}/> 
     <Form />
     </>
   )
