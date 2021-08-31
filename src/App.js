bed08f8ccc305af3d0a0bd28f19b09ae0f2b418b
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-08-31 22:13:54 +0200

    Store Todo in localStorage, load after storing and reload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList'
 import Form from './Form';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
   // todos: every single todo inside the list
@@ -8,10 +11,22 @@ function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
 
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  //STORE NEW TODOS IN LOCAL STORAGE, RESETS ON REFRESH
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos]);
+
   function handleAddTodo(e) {
     const name = todoNameRef.current.value;
     if (name === '') return
-    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false }]
+    })
     todoNameRef.current.value = null; 
   }
 
