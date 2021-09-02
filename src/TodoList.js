024a3b60f112321c023c91732d8f6f5ea4afa7e3
DennisVN <78357110+DennisVN@users.noreply.github.com>
2021-09-02 17:26:20 +0200

    Set new key 'item' through new useState and Localstorage, WIP TOMORROW use The state to save completed tasksk in localStorage

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,22 @@
-import React from 'react';
 import Todo from './Todo';
+import React, { useEffect, useState } from 'react';
+
+const LOCAL_STORAGE_KEY = 'todoApp.item';
 
 // render out Todos
 export default function TodoList({ todos, toggleTodo }) {
+    const [allTodos, setAllTodos] = useState([]);
+
+    useEffect(() => {
+        //TODO : if state is true, SAVE it in localStorage
+        todos.map( item => {
+            if (item.complete) {
+                localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(item))
+            }
+        }) 
+    }, [todos])
+
+
     return (
         todos.map(todo => {
             return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />
